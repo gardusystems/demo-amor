a196a1e3ff579e067a57dd89dbb10960de5baf20
jeanpaul-garcia <jeanpaulgarciaduarte@gmail.com>
2020-07-02 17:25:49 -0500

    02-07-2020"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   {
@@ -11,6 +12,15 @@ const routes: Routes = [
     path: "menu",
     loadChildren: () =>
       import("./pages/menu/menu.module").then((m) => m.MenuPageModule),
+      canActivate: [LoginGuard]
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   },
   {
     path: "**",
--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -8,7 +8,7 @@ import { AlertController } from '@ionic/angular';
 })
 export class HomePage {
   workshops = [{}, {}];
-
+  array = [1,2,3,4,5,6,7,8];
   constructor(public alertController:AlertController) {}
 
   async alertConf() {
