82ccbae00cb9f1eb5ac3d675de0bdb23d30407aa
jeanpaul-garcia <jeanpaulgarciaduarte@gmail.com>
2020-08-10 00:33:29 -0500

    10/08/2020

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,6 +4,7 @@ import { Storage } from "@ionic/storage";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { ModalController } from '@ionic/angular';
 import { ModalPage } from '../pages/modal/modal.page';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: "root",
@@ -11,7 +12,8 @@ import { ModalPage } from '../pages/modal/modal.page';
 export class AuthService {
   constructor(private storage: Storage, private http: HttpClient, 
     public modalController: ModalController) {}
-  API_URL = "http://localhost:8080/api/users/";
+    API_URL = environment.API_URL + "users/";
+    //API_URL = "http://localhost:8080/api/users/";
 
   default = {
     isLoggedIn: false,
@@ -49,7 +51,6 @@ export class AuthService {
                     token: token
                   },
                 };
-                console.log(user)
                 this.userData.next(user);
                 resolve(true);
               } else {
@@ -91,6 +92,26 @@ export class AuthService {
     });
   }
   modal;
+
+  getUsers(token){
+    let formToken = new FormData();
+    formToken.append("token", token.toString());
+    return this.http
+      .post(`${this.API_URL}list`, formToken, {
+        headers: new HttpHeaders(),
+      });
+        }
+
+  editRole(id_user,token){
+    let formData = new FormData();
+    formData.append("token", token.toString());
+    formData.append("id_user", id_user.toString());
+    return this.http
+      .post(`${this.API_URL}edit-role`, formData, {
+        headers: new HttpHeaders(),
+      });
+  }
+
 
   async presentLoginRegisterModal() {
     this.modal = await this.modalController.create({
